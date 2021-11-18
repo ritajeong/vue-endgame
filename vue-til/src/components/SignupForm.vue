5c18041d5a4ff9cff660c2ed14a494d79c625198
ritajeong <litch0s2@gmail.com>
2021-11-19 01:14:59 +0900

    회원가입 기능 구현

--- vue-til/src/components/SignupForm.vue
+++ vue-til/src/components/SignupForm.vue
@@ -1,20 +1,54 @@
 <template>
-  <form>
+  <form v-on:submit.prevent="submitForm">
     <div>
       <label for="username">id: </label>
-      <input id="username" type="text" />
+      <input id="username" type="text" v-model="username" />
     </div>
     <div>
       <label for="password">password: </label>
-      <input id="password" type="text" />
+      <input id="password" type="text" v-model="password" />
     </div>
     <div>
       <label for="nickname">nickname: </label>
-      <input id="nickname" type="text" />
+      <input id="nickname" type="text" v-model="nickname" />
     </div>
+    <button type="submit">회원 가입</button>
+    <p>{{ logMessage }}</p>
   </form>
 </template>
 <script>
-export default {};
+import { registerUser } from '@/api/index';
+
+export default {
+  data() {
+    return {
+      //form value
+      username: '',
+      password: '',
+      nickname: '',
+      //log
+      logMessage: '',
+    };
+  },
+  methods: {
+    async submitForm() {
+      console.log('폼제출');
+      const userDate = {
+        username: this.username,
+        password: this.password,
+        nickname: this.nickname,
+      };
+      const { data } = await registerUser(userDate); //destructuring(구조분해할당 or 비구조화할당)
+      this.logMessage = `${data.username}님이 가입되었습니다.`; //ES6 템플릿 리터럴(백틱)
+      console.log(data.username);
+      this.initForm();
+    },
+    initForm() {
+      this.username = '';
+      this.password = '';
+      this.nickname = '';
+    },
+  },
+};
 </script>
 <style></style>
