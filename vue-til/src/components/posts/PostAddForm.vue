3c997a88b2d7fbbb011fc2f5ba240c2ebd05728e
ritajeong <litch0s2@gmail.com>
2021-11-20 05:47:30 +0900

    노트 생성 기능 구현

--- vue-til/src/components/posts/PostAddForm.vue
+++ vue-til/src/components/posts/PostAddForm.vue
@@ -2,22 +2,54 @@
   <div class="contents">
     <h1 class="page-header">Create Post</h1>
     <div class="form-wrapper">
-      <form class="form">
+      <form class="form" v-on:submit.prevent="submitForm">
         <div>
           <label for="title">Title:</label>
-          <input id="title" type="text" />
+          <input id="title" type="text" v-model="title" />
         </div>
         <div>
           <label for="contents">Contents:</label>
-          <textarea id="contents" type="text" rows="5" />
+          <textarea id="contents" type="text" rows="5" v-model="contents" />
+          <p class="validation-text warning" v-if="!isContentsValid">
+            Contents must be less than 200
+          </p>
         </div>
         <button type="submit" class="btn">Create</button>
       </form>
+      <p>{{ logMessage }}</p>
     </div>
   </div>
 </template>
 <script>
-export default {};
+import { createPost } from '@/api/index';
+export default {
+  data() {
+    return {
+      title: '',
+      contents: '',
+      logMessage: '',
+    };
+  },
+  computed: {
+    isContentsValid() {
+      return this.contents.length <= 200;
+    },
+  },
+  methods: {
+    async submitForm() {
+      try {
+        const response = await createPost({
+          title: this.title,
+          contents: this.contents,
+        });
+        console.log(response);
+      } catch (error) {
+        console.log(error.response.data.message);
+        this.logMessage = '글 등록에 실패했습니다.';
+      }
+    },
+  },
+};
 </script>
 <style scoped>
 .form-wrapper .form {
