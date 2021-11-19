910cbb9464dade1c40a98d63053194db33d21adf
ritajeong <litch0s2@gmail.com>
2021-11-20 03:02:21 +0900

    학습노트 리스트 렌더링

--- vue-til/src/views/MainPage.vue
+++ vue-til/src/views/MainPage.vue
@@ -2,12 +2,44 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
+      <div v-if="isLoading">
+        Loading...
+      </div>
+      <ul>
+        <postListItem
+          v-for="postItem in postItems"
+          v-bind:key="postItem._id"
+          v-bind:postItem="postItem"
+        ></postListItem>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { fetchPosts } from '@/api/index';
+import PostListItem from '../components/posts/PostListItem.vue';
+export default {
+  components: { PostListItem },
+  data() {
+    return {
+      postItems: [],
+      isLoading: false,
+    };
+  },
+  methods: {
+    async fetchData() {
+      this.isLoading = true;
+      const { data } = await fetchPosts();
+      this.isLoading = false;
+      console.log(data.posts);
+      this.postItems = data.posts;
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+};
 </script>
 
 <style></style>
